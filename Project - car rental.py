326ab0343c4a2e0cb8712da69cf624f27865dde8
Antoniel Cleyton <antonieldbms@gmail.com>
2024-05-17 00:52:38 -0300

    Update Project - car rental.py

--- Project - car rental.py
+++ Project - car rental.py
@@ -1,99 +1,282 @@
-import os
 import time
+import os
 
-# Dictionary containing car models and their rental prices
-car_portfolio = {
-    "Chevrolet Tracker": "R$ 120 /day",
-    "Chevrolet Onix": "R$ 90 /day",
-    "Chevrolet Spin": "R$ 150 /day",
-    "Hyundai HB20": "R$ 85 /day",
-    "Hyundai Tucson": "R$ 120 /day",
-    "Fiat Uno": "R$ 60 /day",
-    "Fiat Mobi": "R$ 70 /day",
-    "Fiat Pulse": "R$ 130 /day"
-}
+class Locadora:
 
-# Dictionary containing rented cars
-rented_cars = {
-    "Chevrolet Tracker": "R$ 120 /day",
-    "Chevrolet Onix": "R$ 90 /day",
-    "Chevrolet Spin": "R$ 150 /day",
-    "Hyundai HB20": "R$ 85 /day",
-    "Hyundai Tucson": "R$ 120 /day",
-    "Fiat Uno": "R$ 60 /day",
-    "Fiat Mobi": "R$ 70 /day",
-    "Fiat Pulse": "R$ 130 /day"
-}
+  carros = {
+      '0':  'Chevrolet Tracker - $ 120 /dia',
+      '1': 'Chevrolet Onix - $ 90 /dia',
+      '2': 'Chevrolet Spin - $ 150 /dia',
+      '3': 'Hyundai HB20 - $ 85 /dia',
+      '4': 'Hyundai Tuckson - $ 120 /dia',
+      '5': 'Fiat Uno - $ 60 /dia',
+      '6': 'Fiat Mobi - $ 70 /dia',
+      '7': 'Fiat Pulse - $ 130 /dia'
 
-# Dictionary containing returned cars
-returned_cars = {}
+    }
 
-while True:
-    os.system("cls")  # Clear the console screen
-    print("Welcome to the car rental service!")
-    print("===================================")
-    main_menu = int(input("0 - Show car portfolio, 1 - Rent a car, 2 - Return a car\n"))
-    
-    if main_menu == 0:
-        i = 0
-        for model, price in car_portfolio.items():
-            print(i, "-", model, "-", price)
-            i += 1
-        print("=========================")
-        choice_1 = int(input("1 - CONTINUE | 2 - EXIT\n"))
-        if choice_1 == 1:
-            print("Returning to the main menu...")
-            time.sleep(2)
+  carros_alugados = {}
+
+  def __init__(self):
+    self.nome = 'nome'
+
+
+  def saudacoes(self):
+      os.system('cls')
+      print('-------------------------------')
+      print('Bem-vindo à locadora de carros!')
+      print('-------------------------------')
+      self.nome = str(input('Qual é o seu nome? '))
+      os.system('cls')
+      barra = '='
+      for i in range(10):
+        teste = barra * i
+        print(f'Prazer {self.nome}!')
+        print('Iniciando o sistema...')
+        print(teste)
+        time.sleep(1)
+        os.system('cls')
+      print('Sistema carregado!')
+      time.sleep(1)
+      os.system('cls')  
+
+
+  def portifolio(self):
+    os.system('cls')
+    while True:
+      print('Bem-vindo à locadora de carros!')
+      print('--------------------------------')
+      print('-------Carros para alugar-------')
+      print('--------------------------------')
+      menu = int(input('0 - Mostrar portifólio | 1 - Alugar um carro | 2 - Devolver um carro\nResposta: '))
+      print('Entrando no menu escolhido...')
+      time.sleep(1)
+      os.system('cls')
+      if menu == 0:
+        print('-----PORTIFOLIO DE CARROS-----')
+        for i, n in self.carros.items():
+          print(f'[{i}] - {n}\n')
+        print('------------------------------')
+        escolha = int(input('0 - Continue | 1 - Sair\nResposta: '))  
+        if escolha == 0:
+          print(f'Certo {self.nome}! Voltando para o menu inicial...')
+          time.sleep(1)
+          os.system('cls')
+          continue
         else:
-            print("Exiting the program...")
-            time.sleep(2)
-            quit()
-            
-    if main_menu == 1:
-        i = 0
-        for model, price in car_portfolio.items():
-            print(i, "-", model, "-", price)
-            i += 1    
-        print("=========================")
-        rent_car = int(input("Choose a code: "))
-        if rent_car in range(len(car_portfolio)):
-            days_rented = int(input("For how many days do you want to rent? "))
-            rental_cost = days_rented * 120  # Assuming the rental cost for all cars is the same
-            os.system("cls")
-            print("You have chosen the Chevrolet Tracker for {} days. The total rental cost would be R${}. Do you want to proceed?\n".format(days_rented, rental_cost))
-            finish_contract = int(input("1 - YES | 2 - NO\n"))
-            if finish_contract == 1:
-                selected_car = list(car_portfolio.keys())[rent_car]
-                del car_portfolio[selected_car]
-                del rented_cars[selected_car]
-                returned_cars[selected_car] = "R$ 120 /day"
-                os.system("cls")
-                print("Congratulations! You rented the {} for {} day(s)".format(selected_car, days_rented))
-                print("=====================")
-                choice_1 = int(input("1 - CONTINUE | 2 - EXIT\n"))
-                if choice_1 == 1:
-                    print("Returning to the main menu...")
-                    time.sleep(2)
+          os.system('cls')
+          print(f'Ok {self.nome}! Estamos saindo do programa...')
+          time.sleep(1)
+          break    
+      elif menu == 1:
+        while True:
+            print('-----CARROS PARA ALUGAR-----')
+            for i, n in self.carros.items():
+                print(f'[{i}] - {n}\n')
+            print('------------------------------')
+            escolha = int(input('0 - Continue | 1 - Sair\nResposta: '))
+            if escolha == 0:
+                escolha1 = int(input('Escolha o código do carro:\nResposta: '))
+                if escolha1 == 0:
+                    carro_escolha = self.carros['0']
+                    del self.carros['0']
+                    self.carros_alugados['0'] = 'Chevrolet Tracker - $ 120 /dia'
+                    dias = int(input('Escolha por quantos dias deseja alugar: '))
+                    print(f'Você alugou o {carro_escolha} por {dias}')
+                    valor = 120 * dias
+                    escolha2 = int(input(f'O aluguel totalizara ${valor}. Deseja Continuar? 1-SIM | 2-NÃO\nResposta:  '))
+                    if escolha2 == 1:
+                        print(f'Certo {self.nome}! Voltando para o menu de carros para alugar...')
+                        time.sleep(1)
+                        os.system('cls')
+                        continue
+                    else:
+                        os.system('cls')
+                        print(f'Ok {self.nome}! Voltando para o menu principal...')
+                        time.sleep(1)
+                        break
+                elif escolha1 == 1:
+                    carro_escolha = self.carros['1']
+                    del self.carros['1']
+                    self.carros_alugados['1'] = 'Chevrolet Onix - $ 90 /dia'
+                    dias = int(input('Escolha por quantos dias deseja alugar: '))
+                    print(f'Você alugou o {carro_escolha} por {dias}')
+                    valor = 90 * dias
+                    escolha2 = int(input(f'O aluguel totalizara ${valor}. Deseja Continuar? 1-SIM | 2-NÃO\nResposta:  '))
+                    if escolha2 == 1:
+                        print(f'Certo {self.nome}! Voltando para o menu de carros para alugar...')
+                        time.sleep(1)
+                        os.system('cls')
+                        continue
+                    else:
+                        os.system('cls')
+                        print(f'Ok {self.nome}! Voltando para o menu principal...')
+                        time.sleep(1)
+                        break
+                elif escolha1 == 2:
+                    carro_escolha = self.carros['2']
+                    del self.carros['2']
+                    self.carros_alugados['2'] = 'Chevrolet Spin - $ 150 /dia'
+                    dias = int(input('Escolha por quantos dias deseja alugar: '))
+                    print(f'Você alugou o {carro_escolha} por {dias}')
+                    valor = 90 * dias
+                    escolha2 = int(input(f'O aluguel totalizara ${valor}. Deseja Continuar? 1-SIM | 2-NÃO\nResposta:  '))
+                    if escolha2 == 1:
+                        print(f'Certo {self.nome}! Voltando para o menu de carros para alugar...')
+                        time.sleep(1)
+                        os.system('cls')
+                        continue
+                    else:
+                        os.system('cls')
+                        print(f'Ok {self.nome}! Voltando para o menu principal...')
+                        time.sleep(1)
+                        break
+                elif escolha1 == 3:
+                    carro_escolha = self.carros['3']
+                    del self.carros['3']
+                    self.carros_alugados['3'] = 'Hyundai HB20 - $ 85 /dia'
+                    dias = int(input('Escolha por quantos dias deseja alugar: '))
+                    print(f'Você alugou o {carro_escolha} por {dias}')
+                    valor = 90 * dias
+                    escolha2 = int(input(f'O aluguel totalizara ${valor}. Deseja Continuar? 1-SIM | 2-NÃO\nResposta:  '))
+                    if escolha2 == 1:
+                        print(f'Certo {self.nome}! Voltando para o menu de carros para alugar...')
+                        time.sleep(1)
+                        os.system('cls')
+                        continue
+                    else:
+                        os.system('cls')
+                        print(f'Ok {self.nome}! Voltando para o menu principal...')
+                        time.sleep(1)
+                        break
+                elif escolha1 == 4:
+                    carro_escolha = self.carros['4']
+                    del self.carros['4']
+                    self.carros_alugados['4'] = 'Hyundai Tucson - $ 120 /dia'
+                    dias = int(input('Escolha por quantos dias deseja alugar: '))
+                    print(f'Você alugou o {carro_escolha} por {dias}')
+                    valor = 90 * dias
+                    escolha2 = int(input(f'O aluguel totalizara ${valor}. Deseja Continuar? 1-SIM | 2-NÃO\nResposta:  '))
+                    if escolha2 == 1:
+                        print(f'Certo {self.nome}! Voltando para o menu de carros para alugar...')
+                        time.sleep(1)
+                        os.system('cls')
+                        continue
+                    else:
+                        os.system('cls')
+                        print(f'Ok {self.nome}! Voltando para o menu principal...')
+                        time.sleep(1)
+                        break
+                elif escolha1 == 5:
+                    carro_escolha = self.carros['5']
+                    del self.carros['5']
+                    self.carros_alugados['5'] = 'Fiat Uno - $ 60 /dia'
+                    dias = int(input('Escolha por quantos dias deseja alugar: '))
+                    print(f'Você alugou o {carro_escolha} por {dias}')
+                    valor = 90 * dias
+                    escolha2 = int(input(f'O aluguel totalizara ${valor}. Deseja Continuar? 1-SIM | 2-NÃO\nResposta:  '))
+                    if escolha2 == 1:
+                        print(f'Certo {self.nome}! Voltando para o menu de carros para alugar...')
+                        time.sleep(1)
+                        os.system('cls')
+                        continue
+                    else:
+                        os.system('cls')
+                        print(f'Ok {self.nome}! Voltando para o menu principal...')
+                        time.sleep(1)
+                        break
+                elif escolha1 == 6:
+                    carro_escolha = self.carros['6']
+                    del self.carros['6']
+                    self.carros_alugados['6'] = 'Fiat Mobi - $ 70 /dia'
+                    dias = int(input('Escolha por quantos dias deseja alugar: '))
+                    print(f'Você alugou o {carro_escolha} por {dias}')
+                    valor = 90 * dias
+                    escolha2 = int(input(f'O aluguel totalizara ${valor}. Deseja Continuar? 1-SIM | 2-NÃO\nResposta:  '))
+                    if escolha2 == 1:
+                        print(f'Certo {self.nome}! Voltando para o menu de carros para alugar...')
+                        time.sleep(1)
+                        os.system('cls')
+                        continue
+                    else:
+                        os.system('cls')
+                        print(f'Ok {self.nome}! Voltando para o menu principal...')
+                        time.sleep(1)
+                        break
+                elif escolha1 == 7:
+                    carro_escolha = self.carros['7']
+                    del self.carros['7']
+                    self.carros_alugados['7'] = 'Fiat Pulse - $ 130 /dia'
+                    dias = int(input('Escolha por quantos dias deseja alugar: '))
+                    print(f'Você alugou o {carro_escolha} por {dias}')
+                    valor = 90 * dias
+                    escolha2 = int(input(f'O aluguel totalizara ${valor}. Deseja Continuar? 1-SIM | 2-NÃO\nResposta:  '))
+                    if escolha2 == 1:
+                        print(f'Certo {self.nome}! Voltando para o menu de carros para alugar...')
+                        time.sleep(1)
+                        os.system('cls')
+                        continue
+                    else:
+                        os.system('cls')
+                        print(f'Ok {self.nome}! Voltando para o menu principal...')
+                        time.sleep(1)
+                        break  
+            elif escolha == 1:
+                print(f'Certo {self.nome}! Voltando para o menu inicial...')
+                time.sleep(1)
+                os.system('cls')
+                break
+            else:
+                print(f'{self.nome}, você fez uma escolha invalida! Tente novamente...')
+                time.sleep(1)
+                os.system('cls')
+                continue
+      elif menu == 2:
+          while True:
+            print('-----DEVOLUÇÃO DE CARRO-----')
+            for i, n in self.carros_alugados.items():
+                print(f'[{i}] - {n}\n')
+            print('----------------------------')
+            escolha = int(input('0 - Continue | 1 - Sair\nResposta: '))
+            if escolha == 0:
+                escolha1 = int(input('Escolha o código do carro:\nResposta: '))
+                carro_escolha = self.carros_alugados[str(escolha1)]
+                self.carros[str(escolha1)] = self.carros_alugados[str(escolha1)]
+                del self.carros_alugados[str(escolha1)]
+                print(f'{self.nome} você devolveu o {carro_escolha}!')
+                print('---------------------------------------------')
+                if not self.carros_alugados:
+                    print('Não há mais carros para devolver...')
                 else:
-                    print("Exiting the program...")
-                    time.sleep(2)
-                    quit()
-        else:
-            print("Invalid car code! Please select a valid option.")
-            time.sleep(2)
-            continue
-            
-    if main_menu == 2:
-        i = 0
-        for model, price in returned_cars.items():
-            print(i, "-", model, "-", price)
-            i += 1
-        print("===================")
-        return_car = int(input("Which car do you want to return? "))
-        car_list = list(returned_cars)
-        car_to_return = car_list[return_car]
-        car_portfolio[car_to_return] = returned_cars[car_to_return]
-        del returned_cars[car_to_return]
-        print("You have returned the {}! Thank you for choosing us.".format(car_to_return))
-        print("Returning to the main menu...")
-        time.sleep(4)
+                    print('Ainda há carros para realizar devolução...')
+                print('---------------------------------------')
+                escolha2 = int(input('Deseja realizar a devolução de outro carro? 0 - SIM | 1 - NÃO\nResposta: '))
+                if escolha2 == 0:
+                    print(f'Certo {self.nome}! Estamos voltando para o menu inicial...')
+                    time.sleep(1)
+                    os.system('cls')
+                    continue
+                else:
+                    print(f'Ok {self.nome}! Voltando para o menu inicial...')
+                    time.sleep(1)
+                    os.system('cls')
+                    break
+            elif escolha == 1:
+                print(f'Ok {self.nome}! Voltando para o menu inicial...')
+                time.sleep(1)
+                os.system('cls')
+                break
+            else:
+                print(f'{self.nome} você escolheu uma opção invalida! Voltando para as opções...')
+                time.sleep(1)
+                os.system('cls')
+                continue
+
+
+
+
+user = Locadora()
+
+user.saudacoes()
+
+user.portifolio()
